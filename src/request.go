8202fa9497e571c21040052748400dbe03074ce0
Mxngls <Maximilian.e.hoenig@gmail.com>
2022-06-07 18:14:25 +0900

    Fix failed last commit

--- src/request.go
+++ src/request.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func requestView(id string, lang string, langCode string, client *http.Client) (resp *http.Response) {
+func requestView(id string, lang string, langCode string, client *http.Client) (resp *http.Response, err error) {
 
 	url := fmt.Sprintf("https://krdict.korean.go.kr/%s/dicSearch/SearchView?ParaWordNo=%s&nation=%s&nationCode=%s&viewType=A&blockCount=10&viewTypes=on",
 		lang,
